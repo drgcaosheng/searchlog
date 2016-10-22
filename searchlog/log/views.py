801c9f0f873a116ceb3e40502293594b4060af5a
drgcaosheng <drgcaosheng@163.com>
2016-10-22 14:22:06 +0800

    searchlog_bootstrap
    searchlog_bootstrap

--- searchlog/log/views.py
+++ searchlog/log/views.py
@@ -1,60 +1,86 @@
 from django.shortcuts import render
 from django.shortcuts import render_to_response
-from django.http import HttpResponse
+from django.http import HttpResponse,Http404
 from django import forms
 from django.contrib import auth
 from django.http import HttpResponseRedirect
+import datetime
+from django.contrib.admin import widgets
+import subprocess
+import os.path
+
 
 class loginForm(forms.Form):
     username = forms.CharField(max_length=30)
     password = forms.CharField(widget=forms.PasswordInput)
 
 class searchForm(forms.Form):
-    searchform = forms.CharField(min_length=5)
+    searchform = forms.CharField(label="Please Enter Search Content",min_length=1)
+    day = forms.DateTimeField(label='Please Enter Log Date',widget=widgets.AdminDateWidget())
 
 def login(request):
-    error = True
+    error = False
     if request.method == 'POST':
+        #print 'ttttt'
         form = loginForm(request.POST)
+        #print '223333333333333333333'
         if form.is_valid():
+            #print '222222222'
             username = request.POST.get('username','')
             password = request.POST.get('password','')
-            print username
-            print password 
             user = auth.authenticate(username=username,password=password)
             #uf = form.cleaned_data
-            #print uf
             if user is not None and user.is_active:
-            
+                #print '1'
                 auth.login(request,user)
                 return HttpResponseRedirect("/loginsuccess/")
             else:
-                error = True
+                #print '2'
+                error=True  
                 #return HttpResponse("not user")
-                return render_to_response('login.html',{'form':form,'error':error})
-            #return HttpResponse("OK")
     else:
-        error = False
         form = loginForm()
-    	return render_to_response('login.html',{'form':form,'error':error})
+    return render_to_response('login.html',{'form':form,'error':error})
 
 def loginsuccess(request):
     if request.user.is_authenticated():
         if request.method == 'POST':
             searchform = searchForm(request.POST)
             if searchform.is_valid():
-                print searchform.cleaned_data
-                return HttpResponse('searchsuccess')
+                searchtext = searchform.cleaned_data['searchform']
+                today = searchform.cleaned_data['day']
+                today_day = str(today.day).zfill(2)
+                #print today_day
+                #print searchtext
+                if '|' in searchtext:
+                    searchtext_error = True
+                    return render_to_response('search.html',{'form':searchform,'searchtext_error':searchtext_error})
+                logpath = os.path.join('/home/var/log/',str(today.year),str(today.month),today_day)
+                if not os.path.exists(logpath):
+                    file_error = True
+                    return render_to_response('search.html',{'form':searchform,'file_error':file_error})
+                logpath = os.path.join(logpath,'*')
+                cmd = "sudo grep %s %s"%(searchtext,logpath)
+                #print cmd
+                showlog = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,stdin=subprocess.PIPE)
+                text = showlog.communicate()
+                textshow = text[0].split('\n')
+                #print textshow
+                return render_to_response('search.html',{'showlog':textshow,'form':searchform})
+                #return render_to_response('showlog.html',{'showlog':textshow})
+                #return HttpResponse(textshow)
         else:
-            searchform = searchForm()
+            searchform = searchForm(
+                initial={'day':datetime.datetime.now().strftime("%Y-%m-%d")}
+            )
         return render_to_response('search.html',{'form':searchform})
+        
         #return HttpResponse("OK")
     else:
-        #error = True
-        #form = loginForm()
-        return HttpResponseRedirect("/login/")
         #return HttpResponse("NO")
+        return HttpResponseRedirect("/login/")
 
 def logout(request):
     auth.logout(request)
-    return HttpResponse("quit")
+    #return HttpResponse("quit")
+    return render_to_response("logout.html")
--- searchlog/searchlog/urls.py
+++ searchlog/searchlog/urls.py
@@ -1,7 +1,7 @@
 """searchlog URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.10/topics/http/urls/
+    https://docs.djangoproject.com/en/1.8/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -10,28 +10,26 @@ Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+    1. Add an import:  from blog import urls as blog_urls
+    2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-#from django.conf.urls import url
 from django.conf.urls import include, url
 from django.contrib import admin
+from log import views
 from django.conf import settings
-from log import views
-'''
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^index/', views.hello),
-    url(r'^login/', views.login),
-]
+#from django.conf import settings
+#from django.contrib.staticfiles import views
 
-from django.conf.urls import include, url
-from django.contrib import admin
-from log import views
-'''
+#def i18n_javascript(request):
+#    return admin.site.i18n_javascript(request)
+
 urlpatterns = [
+#    url(r'^admin/jsi18n',i18n_javascript),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$',views.login),
     url(r'^login/$',views.login),
     url(r'^loginsuccess/$',views.loginsuccess),
     url(r'^logout/$',views.logout),
+    url(r'^site_media/(?P<path>.*)$', 'django.views.static.serve',{'document_root': settings.MEDIA_ROOT }),
+#    url(r'static/(?P<path>.*$',views.serve),
 ]
